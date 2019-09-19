1d159f39ef213335743d05cb3ebc5449741f7190
Highstarrr <36108071+Highstarrr@users.noreply.github.com>
2019-09-19 21:31:40 +0800

    Add files via upload

--- test/DoFile.java
+++ test/DoFile.java
@@ -1,7 +1,6 @@
 package test;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,120 +8,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DoFile {
-	private static final String path = "./test.json";
-	private static final String dataPath = "./data.txt";
-	private static final String separator = System.getProperty("line.separator");
+private static final String path = "./test.json";
+private static final String dataPath = "./data.txt";
+private static final String separator = System.getProperty("line.separator");
 
 	
-	//保存文件
-	public static void saveFile(List<User> list) {
+//保存文件
+public static void saveFile(List<User> list) throws IOException {
 		
-		File file = new File(path);
-		FileWriter fw = null ;
-		if(!file.exists()){
-			try {
-				file.createNewFile();
-			} catch (IOException e) {
-				
-				e.printStackTrace();
-			}
-		}
-		
-		try {
-			fw = new FileWriter(file);
-			for(User a : list) {
-				fw.write("{");
-				fw.write("\"" + "姓名" + "\"" + ":");
-				fw.write("\"" + a.getName() + "\"" + ",");
-				fw.write("\"" + "手机" + "\"" + ":");
-				fw.write("\"" + a.getPhone() + "\"" + ",");
-				fw.write("\"" + "地址" + "\"" + ":");
-				fw.write("\"" + a.getAl().toString() + "\"");
-				fw.write("}");
-				fw.write(separator);
-				
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				fw.flush();
-				fw.close();
-			} catch (IOException e) {
-				
-				e.printStackTrace();
-			}
-		}
+	File f = new File(path);
+	FileWriter fw = null ;
+	f.createNewFile();		
+	fw = new FileWriter(f);
+	for(User a : list) {
+		fw.write("{");
+		fw.write("\"" + "姓名" + "\"" + ":");
+		fw.write("\"" + a.getName() + "\"" + ",");
+		fw.write("\"" + "手机" + "\"" + ":");
+		fw.write("\"" + a.getPhone() + "\"" + ",");
+		fw.write("\"" + "地址" + "\"" + ":");
+		fw.write("\"" + a.getAl().toString() + "\"");
+		fw.write("}");
+		fw.write(separator);
 	}
+		fw.flush();
+		fw.close();
+}
 	
-	
-	public static List<String> readFile() {
-		@SuppressWarnings("unused")
-		File file = new File(dataPath);
-		FileReader fd = null ;
-		BufferedReader br = null ; 
-		List<String> dataList = new ArrayList<>();
-		try {
-			 fd = new FileReader(dataPath);
-			 br = new BufferedReader(fd);
-			 String line ;
-			 while((line = br.readLine()) != null) {
-				 dataList.add(line);
-			 }
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				br.close();
-				fd.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}	
-		}
+public static List<String> readFile() throws IOException {
+	@SuppressWarnings("unused")
+	File file = new File(dataPath);
+	FileReader fd = new FileReader(dataPath);
+	BufferedReader br = new BufferedReader(fd);
+	List<String> dataList = new ArrayList<>();
 		
-		return dataList ;
+	String line ;
+	while((line = br.readLine()) != null) {
+		dataList.add(line);
 	}
+	br.close();
+	fd.close();
+			
+	return dataList ;
+}
 	
-	//方法重载
-	public static List<String> readFile(String filepath) {
-		File file = null;
-		if(filepath != null) {
-			file = new File(filepath);
-		}else {
-			file = new File(dataPath);
-		}
-		
-		
-		FileReader fr = null ;
-		BufferedReader br = null ; 
-		List<String> dataList = new ArrayList<>();
-		if(!file.exists()) {
-			return null;  //说明没有存在data文件
-		}
-		
-		try {
-			 fr = new FileReader(dataPath);
-			 br = new BufferedReader(fr);
-			 String tt ;
-			 while((tt = br.readLine()) != null) {
-				 dataList.add(tt);
-			 }
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				br.close();
-				fr.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return dataList ;
+public static List<String> readFile(String filepath) throws IOException {
+	@SuppressWarnings("unused")
+	File file = null;
+	if(filepath != null) {
+		file = new File(filepath);
+	}else {
+		file = new File(dataPath);
 	}
+		
+	FileReader fr = new FileReader(dataPath);
+	BufferedReader br = new BufferedReader(fr);
+	List<String> dataList = new ArrayList<>();
+	String re ;
+	while((re = br.readLine()) != null) {
+		dataList.add(re);
+	}
+	br.close();
+	fr.close();
+		
+	return dataList ;
+}
 	
 }
