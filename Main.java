a05919483320e8a47d881dee4c2aaa22faf740e3
Highstarrr <36108071+Highstarrr@users.noreply.github.com>
2019-09-21 08:14:42 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.io.IOException;
+import java.test.*;
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		new Do(args[0],args[1]);
+		
+	}
+	
+}
