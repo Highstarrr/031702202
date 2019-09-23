8431e1725e066794cff853559d5f1003c6abee57
Highstarrr <36108071+Highstarrr@users.noreply.github.com>
2019-09-23 22:44:10 +0800

    Add files via upload

--- test/Do.java
+++ test/Do.java
@@ -4,24 +4,19 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Do {
- 
-	public Do() throws IOException {
-
-
-		List<String> dataList = DoFile.readFile();
+	
+	public Do(String args0,String args1) throws IOException {
+		//∂¡»°
+		List<String> dataList = DoFile.readFile(args0);
+		
 		List<User> addList = new ArrayList<>();
 		for(String data : dataList) {
-
 			addList.add(new User(data));
-
 		}
-
-		for(User add : addList) { System.out.println(add); }
-
+		
 		//for(User add : addList) { System.out.println(add); }
-		DoFile.saveFile(addList); 
-
+		//±£¥Ê
+		DoFile.saveFile(addList,args1); 
 	}
 }
